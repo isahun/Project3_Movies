729071513c3110d58a137cab1b9dc0f24a69296a
Irene Virgínia <isahun7@gmail.com>
2026-04-25 22:58:32 +0200

    feat: generate ng components for main pages

---
+++ src/app/pages/director-detail/director-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-detail',
+  imports: [],
+  templateUrl: './director-detail.html',
+  styleUrl: './director-detail.css',
+})
+export class DirectorDetail {}
