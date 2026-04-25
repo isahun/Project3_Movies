c2634c0589ca4eade7666d09d5c6e94225d9a177
Irene Virgínia <isahun7@gmail.com>
2026-04-26 00:58:29 +0200

    feat: fill movie interface with data structure learned through fetched data from tmdb api

--- src/app/interfaces/movie.ts
+++ src/app/interfaces/movie.ts
@@ -1,1 +1,10 @@
-export interface Movie {}
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  original_language: string;
+  vote_average: number;
+  vote_count: number;
+}
