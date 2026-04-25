0019dd79227581c875581aca31581866cf86314f
Irene Virgínia <isahun7@gmail.com>
2026-04-26 00:59:14 +0200

    feat: setup movies-list to check api calls work and paint movies in ui

--- src/app/pages/movies-list/movies-list.ts
+++ src/app/pages/movies-list/movies-list.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TmdbService } from '../../services/tmdb-service';
+import { Movie } from '../../interfaces/movie';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.css',
 })
-export class MoviesList {}
+export class MoviesList {
+  private tmdbService = inject(TmdbService);
+  movies: Movie[] = [];
+
+  ngOnInit(): void {
+    this.tmdbService.getMovies().subscribe({
+      next: (data) => {
+        //console.log(data);
+        this.movies = data
+      },
+      error: (err) => console.error(err)
+    });
+
+  }
+}
