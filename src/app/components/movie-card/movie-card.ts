c755be0568be88ed1a70d417cf949167cf1a0d32
Irene Virgínia <isahun7@gmail.com>
2026-04-27 22:20:46 +0200

    feat(movie-card): add routerLink to navigate to movie detail

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Movie } from '../../interfaces/movie';
 import { DatePipe, DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [DatePipe, DecimalPipe],
+  imports: [DatePipe, DecimalPipe, RouterLink],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css',
 })
