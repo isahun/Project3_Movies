889cc94cc7db1ad62f3307b993b8c13fe27be9bd
Irene Virgínia <isahun7@gmail.com>
2026-04-27 20:21:29 +0200

    chore: remove unused imports from movies-list, now used in movie-card due to refactorization

--- src/app/pages/movies-list/movies-list.ts
+++ src/app/pages/movies-list/movies-list.ts
@@ -1,13 +1,12 @@
 import { MoviesService } from './../../services/movies-service';
 import { Component, inject } from '@angular/core';
-import { Movie } from '../../interfaces/movie';
 import { DatePipe } from '@angular/common';
 import { SearchTool } from '../../components/search-tool/search-tool';
 import { MovieCard } from '../../components/movie-card/movie-card';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [DatePipe, SearchTool, MovieCard],
+  imports: [SearchTool, MovieCard],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.css',
 })
