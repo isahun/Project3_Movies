36ae24297a0b41f07d3946f57c93fe180c957192
Irene Virgínia <isahun7@gmail.com>
2026-04-27 22:20:36 +0200

    feat(interfaces): add PersonDetail, CastMember, CrewMember and Credits interfaces

---
+++ src/app/interfaces/person-detail.ts
@@ -0,0 +1,30 @@
+export interface PersonDetail {
+  id: number;
+  name: string;
+  biography: string;
+  birthday: string | null;
+  place_of_birth: string | null;
+  profile_path: string | null;
+  known_for_department: string;
+}
+
+export interface CastMember {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+  order: number;
+}
+
+export interface CrewMember {
+  id: number;
+  name: string;
+  job: string; 
+  department: string;
+  profile_path: string | null;
+}
+
+export interface Credits {
+  cast: CastMember[];
+  crew: CrewMember[];
+}
