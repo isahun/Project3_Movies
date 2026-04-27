7be7c9532382f5fa7d97e361e32e55c0877fbab3
Irene Virgínia <isahun7@gmail.com>
2026-04-27 20:16:43 +0200

    feat(movie-card): create card component with poster, year and score

--- src/app/pages/movies-list/movies-list.ts
+++ src/app/pages/movies-list/movies-list.ts
@@ -3,10 +3,11 @@ import { Component, inject } from '@angular/core';
 import { Movie } from '../../interfaces/movie';
 import { DatePipe } from '@angular/common';
 import { SearchTool } from '../../components/search-tool/search-tool';
+import { MovieCard } from '../../components/movie-card/movie-card';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [DatePipe, SearchTool],
+  imports: [DatePipe, SearchTool, MovieCard],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.css',
 })
