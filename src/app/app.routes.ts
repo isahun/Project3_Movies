1afcdee17a6d211a74933ea30ea20a490727ecd6
Irene Virgínia <isahun7@gmail.com>
2026-04-27 21:10:03 +0200

    feat(routes): add movie-detail route with id param

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Shell } from './pages/shell/shell';
 import { Home } from './pages/home/home';
 import { MoviesList } from './pages/movies-list/movies-list';
-import { MovieDetail } from './pages/movie-detail/movie-detail';
+import { MovieDetail } from './pages/movie-detail/movie-detail-page';
 import { Favorites } from './pages/favorites/favorites';
 import { UserProfile } from './pages/user-profile/user-profile';
 import { UserLogin } from './pages/user-login/user-login';
