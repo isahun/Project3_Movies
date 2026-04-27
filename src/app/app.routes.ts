3cee0edc0486eae71e29935bcafbf514314f4fbf
Irene Virgínia <isahun7@gmail.com>
2026-04-27 22:20:58 +0200

    fix(routes): rename MovieDetail to MovieDetailPage in route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Shell } from './pages/shell/shell';
 import { Home } from './pages/home/home';
 import { MoviesList } from './pages/movies-list/movies-list';
-import { MovieDetail } from './pages/movie-detail/movie-detail-page';
+import { MovieDetailPage } from './pages/movie-detail/movie-detail-page';
 import { Favorites } from './pages/favorites/favorites';
 import { UserProfile } from './pages/user-profile/user-profile';
 import { UserLogin } from './pages/user-login/user-login';
@@ -16,7 +16,7 @@ export const routes: Routes = [{
   component: Shell, children: [
     { path: '', component: Home },
     { path: 'movies', component: MoviesList },
-    { path: 'movies/:movieId', component: MovieDetail },
+    { path: 'movies/:movieId', component: MovieDetailPage },
     { path: 'actor/:actorId', component: ActorDetail },
     { path: 'director/:directorId', component: DirectorDetail },
     { path: 'favorites', component: Favorites },
