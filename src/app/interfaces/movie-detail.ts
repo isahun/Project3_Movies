ff7dc3b394d010e8d5d7cfc99455bbfcca655c24
Irene <isahun7@gmail.com>
2026-04-28 13:28:26 +0200

    feat(interfaces): add Video interface

--- src/app/interfaces/movie-detail.ts
+++ src/app/interfaces/movie-detail.ts
@@ -10,3 +10,12 @@ export interface MovieDetail {
   tagline: string;
   genres: { id: number; name: string }[];
 }
+
+export interface Video {
+  id: string;
+  key: string;
+  name: string;
+  site: string;
+  type: string;
+  official: boolean;
+}
