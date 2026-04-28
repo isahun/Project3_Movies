85f59d3d77eee2d350da51d19a94373c41454047
Irene <isahun7@gmail.com>
2026-04-28 13:29:02 +0200

    feat(movie-detail): embed YouTube trailer using DomSanitizer

--- src/app/pages/movie-detail/movie-detail-page.ts
+++ src/app/pages/movie-detail/movie-detail-page.ts
@@ -2,10 +2,11 @@ import { Component, input, inject, OnInit, signal } from '@angular/core';
 import { TmdbService } from '../../services/tmdb-service';
 import { forkJoin } from 'rxjs';
 import { CastMember, CrewMember } from '../../interfaces/person-detail';
-import { MovieDetail } from '../../interfaces/movie-detail';
+import { MovieDetail, Video } from '../../interfaces/movie-detail';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { buildVideoEmbedUrl } from '../../utils/video-url.utils';
 @Component({
   selector: 'app-movie-detail-page',
   imports: [DecimalPipe, DatePipe, RouterLink],
@@ -13,12 +14,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './movie-detail-page.css',
 })
 export class MovieDetailPage implements OnInit {
-  movieId = input.required<string>();
   private tmdbService = inject(TmdbService);
+  private sanitizer = inject(DomSanitizer);
+  movieId = input.required<string>();
 
   movie = signal<MovieDetail | null>(null);
   cast = signal<CastMember[]>([]);
   director = signal<CrewMember | null>(null);
+  trailerUrl: SafeResourceUrl | null = null;
+
   isLoading = signal(true);
 
   ngOnInit() {
@@ -27,17 +31,38 @@ export class MovieDetailPage implements OnInit {
     forkJoin({
       movie: this.tmdbService.getMovieById(movieId),
       credits: this.tmdbService.getMovieCredits(movieId),
+      videos: this.tmdbService.getMovieVideo(movieId),
     }).subscribe({
-      next: ({ movie, credits }) => {
+      next: ({ movie, credits, videos }) => {
         this.movie.set(movie);
         this.cast.set(credits.cast.slice(0, 10));
         this.director.set(credits.crew.find((person) => person.job === 'Director') ?? null);
+        console.log('Videos disponibles:', videos);
+
+        const trailer =
+          videos.find(
+            (video) =>
+              (video.type === 'Trailer' || video.type === 'Teaser') &&
+              video.site === 'YouTube' &&
+              video.official,
+          ) ??
+          videos.find(
+            (video) =>
+              (video.type === 'Trailer' || video.type === 'Teaser') && video.site === 'YouTube',
+          );
+
+        if (trailer) {
+          const unsanitizedUrl = buildVideoEmbedUrl(trailer.key, trailer.site);
+          if (unsanitizedUrl)
+            this.trailerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(unsanitizedUrl);
+        }
+
         this.isLoading.set(false);
       },
       error: (err) => {
         console.error('error:', err);
         this.isLoading.set(false);
-      }
+      },
     });
   }
 }
