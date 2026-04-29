648e867b1ba9eb3212ebad0ef55949b50c441f76
Irene Virgínia <isahun7@gmail.com>
2026-04-29 22:33:55 +0200

    style(favorites): add grid layout, rating selector and empty state

--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FavoritesService } from '../../services/favorites-service';
 import { MovieCard } from '../../components/movie-card/movie-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [MovieCard],
+  imports: [MovieCard, RouterLink],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
