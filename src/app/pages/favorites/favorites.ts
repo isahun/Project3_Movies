0b99d587b14067a8131b451ecb6919497af8d033
Irene Virgínia <isahun7@gmail.com>
2026-04-29 20:51:01 +0200

    feat(favorites): implement favorites page with rating selector and ranking

--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FavoritesService } from '../../services/favorites-service';
+import { MovieCard } from '../../components/movie-card/movie-card';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [MovieCard],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
-export class Favorites {}
+export class Favorites implements OnInit {
+  favoritesService = inject(FavoritesService);
+
+  ngOnInit(): void {
+    this.favoritesService.loadFavorites();
+  }
+}
