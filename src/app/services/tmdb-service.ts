da8deb8eaa38c4421384efaaeff1f7ce50e3dd43
Irene <isahun7@gmail.com>
2026-04-29 12:49:15 +0200

    feat(tmdb-service): add getMovieWatchProviders and getPersonMovieCredits

--- src/app/services/tmdb-service.ts
+++ src/app/services/tmdb-service.ts
@@ -3,9 +3,10 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Movie } from '../interfaces/movie';
-import { MovieDetail } from '../interfaces/movie-detail';
-import { Credits, PersonDetail } from '../interfaces/person-detail';
+import { MovieDetail, WatchProviderResult } from '../interfaces/movie-detail';
+import { Credits, PersonDetail, PersonMovieCredit } from '../interfaces/person-detail';
 import { Video } from '../interfaces/movie-detail';
+import { PersonMovieCredits } from '../interfaces/person-detail';
 
 @Injectable({
   providedIn: 'root',
@@ -68,5 +69,27 @@ export class TmdbService {
         map((response) => response.results),
         catchError(this.handleError.bind(this)),
       );
+  }
+
+  getMovieWatchProviders(
+    movieId: number,
+    region: string = 'ES',
+  ): Observable<WatchProviderResult | null> {
+    return this.http
+      .get<{
+        id: number;
+        results: { [region: string]: WatchProviderResult };
+      }>(`${this.apiUrl}/movie/${movieId}/watch/providers`, { headers: this.headers })
+      .pipe(
+        map((response) => response.results[region] ?? null),
+        catchError(this.handleError.bind(this)),
+      );
+  }
+
+  getPersonMovieCredits(personId: number): Observable<PersonMovieCredits> {
+    return this.http
+    .get<PersonMovieCredits>
+    (`${this.apiUrl}/person/${personId}/movie_credits`, { headers: this.headers })
+    .pipe(catchError(this.handleError.bind(this)));
   }
 }
