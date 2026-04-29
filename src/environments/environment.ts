bdab29476c4ba9a8455c8aa8fe5a03ba680e782c
Irene <isahun7@gmail.com>
2026-04-29 13:28:51 +0200

    feat(environment): add Supabase project credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,10 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.themoviedb.org/3',
   apiKey: '8223da45e0bdd7a6309e9cb5d0bb6105',
-  accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjIzZGE0NWUwYmRkN2E2MzA5ZTljYjVkMGJiNjEwNSIsIm5iZiI6MTc3NzE1MzM4MS43NDgsInN1YiI6IjY5ZWQzNTY1YjllOTNiYTg5OGI0ZWJjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U3HpfaGNVk03T6MViZpnte29WkD3E7WI9vHJmCLQEhw'
-}
+  accessToken:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjIzZGE0NWUwYmRkN2E2MzA5ZTljYjVkMGJiNjEwNSIsIm5iZiI6MTc3NzE1MzM4MS43NDgsInN1YiI6IjY5ZWQzNTY1YjllOTNiYTg5OGI0ZWJjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U3HpfaGNVk03T6MViZpnte29WkD3E7WI9vHJmCLQEhw',
+  supabase: {
+    sbUrl: 'https://osqpajwsxforocukyyiw.supabase.co',
+    sbKey: 'sb_publishable_5dBy-UEVlECXsM-7e8EADw_1pZV_Eqj',
+  },
+};
